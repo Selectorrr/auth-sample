92381dc7e877d38b7996a7708ee5b82dca3b564e
stepan litvinov <stepan_litvinov@epam.com>
2019-04-24 17:24:43 +0300

    google example

---
+++ src/main/java/site/selector/sso/web/HelloController.java
@@ -0,0 +1,18 @@
+package site.selector.sso.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Stepan Litvinov on 24/04/2019.
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/me")
+    public ResponseEntity<OAuth2AuthenticationToken> hello(OAuth2AuthenticationToken currentUser) {
+        return ResponseEntity.ok(currentUser);
+    }
+}
